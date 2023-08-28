e2c694a3469cbec235ab270334c2cba310a2e730
Appa Computer <joppukutten@gmail.com>
2023-08-28 18:54:54 +0530

    28Aug-Update-3

--- Xenon.py
+++ Xenon.py
@@ -1,56 +1,83 @@
 import pygame as pg
-import sys  
+import sys
 
 
 from classes import *
 
 
-
-
 class App:
-    def __init__(self,x,y,name = "App"):
-        self.screen = pg.display.set_mode((x,y))
+    def __init__(self, x, y, name="App"):
+        self.screen = pg.display.set_mode((x, y))
         pg.display.set_caption(name)
         self.clock = pg.time.Clock()
         self.running = True
         self.fps = 60
-        self.background_color = (255,255,255)
+        self.background_color = (255, 255, 255)
         self.widgets = []
         self.widget_count = 0
-        
-    def set_background_color(self,color):
+
+    def set_background_color(self, color):
         self.background_color = color
 
-    def set_fps(self,fps):
+    def set_fps(self, fps):
         self.fps = fps
-    
-    def set_caption(self,name):
+
+    def set_caption(self, name):
         pg.display.set_caption(name)
 
-    def add_label(self,*,text,pos,color = (0,0,0),font = "Arial",size = 20):
-        self.widgets.append(Label(text,pos,color,font,size))
+    def add_label(self, *, text, pos, color=(0, 0, 0), font="Arial", size=20):
+        self.widgets.append(Label(text, pos, color, font, size))
         self.widgets[-1].id = self.widget_count
         self.widget_count += 1
         return self.widgets[-1]
 
-    def add_button(self,*,text,pos,color = (0,0,0),bgColor = 'lightgray',outlineColor = 'grey',font = "Arial",size = 20,action = None):
-        self.widgets.append(Button(text,pos,color,bgColor,outlineColor,font,size,action))
+    def add_button(
+        self,
+        *,
+        text,
+        pos,
+        color=(0, 0, 0),
+        bgColor="lightgray",
+        outlineColor="grey",
+        font="Arial",
+        size=20,
+        action=None
+    ):
+        self.widgets.append(
+            Button(text, pos, color, bgColor, outlineColor, font, size, action)
+        )
         self.widgets[-1].id = self.widget_count
         self.widget_count += 1
         return self.widgets[-1]
 
-    def add_image(self,*,image,pos):
-        self.widgets.append(Image(image,pos))
+    def add_image(self, *, image, pos):
+        self.widgets.append(Image(image, pos))
         self.widgets[-1].id = self.widget_count
         self.widget_count += 1
         return self.widgets[-1]
 
-    def add_textbox(self,*,text,size,pos,color = 'black',activeColor = 'white',bgColor = 'lightgray',outlineColor = 'grey',font = "Arial",fsize = 20):
-        self.widgets.append(TextBox(text,size,pos,color,activeColor,bgColor,outlineColor,font,fsize))
+    def add_textbox(
+        self,
+        *,
+        text,
+        size,
+        pos,
+        color="black",
+        activeColor="white",
+        bgColor="lightgray",
+        outlineColor="grey",
+        font="Arial",
+        fsize=20
+    ):
+        self.widgets.append(
+            TextBox(
+                text, size, pos, color, activeColor, bgColor, outlineColor, font, fsize
+            )
+        )
         self.widgets[-1].id = self.widget_count
         self.widget_count += 1
         return self.widgets[-1]
-    
+
     def run(self):
         while self.running:
             for event in pg.event.get():
@@ -59,11 +86,11 @@ class App:
                     sys.exit()
                 if event.type == pg.MOUSEBUTTONDOWN:
                     for widget in self.widgets:
-                        if isinstance(widget,Button) or isinstance(widget,TextBox):
+                        if isinstance(widget, Button) or isinstance(widget, TextBox):
                             widget.check_click(self)
                 if event.type == pg.KEYDOWN:
                     for widget in self.widgets:
-                        if isinstance(widget,TextBox):
+                        if isinstance(widget, TextBox):
                             if widget.active:
                                 if event.key == pg.K_BACKSPACE:
                                     widget.set_text(widget.text[:-1])
@@ -71,8 +98,10 @@ class App:
                                     widget.active = False
                                     print(widget.link)
                                     if widget.link != None:
-                                        if isinstance(widget.link,TextBox): widget.link.active = True
-                                        elif isinstance(widget.link,Button): widget.link.action()
+                                        if isinstance(widget.link, TextBox):
+                                            widget.link.active = True
+                                        elif isinstance(widget.link, Button):
+                                            widget.link.action()
                                         break
                                 else:
                                     if widget.numbers_only:
@@ -80,7 +109,7 @@ class App:
                                             widget.set_text(widget.text + event.unicode)
                                     else:
                                         widget.set_text(widget.text + event.unicode)
-            
+
             self.screen.fill(self.background_color)
             for widget in self.widgets:
                 if not widget.hidden:
@@ -90,5 +119,7 @@ class App:
 
 
 if __name__ == "__main__":
-    print("Xenon.py is not meant to be run as a standalone program. Please import it into your program.")
+    print(
+        "Xenon.py is not meant to be run as a standalone program. Please import it into your program."
+    )
     sys.exit()
